44f36c9e298684e17e4395683b9af6cc54045632
Kalpesh Naik <kalpeshsnaik09@gmail.com>
2020-02-23 15:29:28 +0530

    add eda packeges

---
+++ EDA.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Checking Normal Distubution
+from scipy.stats import shapiro
+from scipy.stats import skew
+from scipy.stats import normaltest
+from scipy.stats import kurtosis
+
+
+def cont_numeric(df,col):
+    '''
+    plot the histogram and boxplt of given column
+
+    param:
+        df  : Dataset Variable
+        col : Column Name
+    
+    return:
+        None
+
+    print:
+        mean,median,Q1,Q3,min,max,skewness,kurtosis,shapiro test,normality test
+
+    display:
+        show plots
+    '''
+
+    mean=df[col].mean()
+    median=df[col].median()
+    Q1=np.nanpercentile(df[col],0.25)
+    Q3=np.nanpercentile(df[col],0.75)
+    min=df[col].min()
+    max=df[col].max()
+
+    Skewness=skew(df[col])
+    Kurtosis=kurtosis(df[col])
+    Shapiro=shapiro(df[col])
+    #z_statistics,p=normaltest(df[col])
+    normality_test=normaltest(df[col])
+    # if p<0.05:
+    #     normality_test='reject null hypothesis (H0)'
+    # else:
+    #     normality_test='faild to reject null hypothesis assume alternate hypothesis correct'
+    
+    print('Column Name : {}'.format(col))
+    print('Mean : {}'.format(mean))
+    print('Median : {}'.format(median))
+    print('Quartile 1 : {}'.format(Q1))
+    print('Quartile 3 : {}'.format(Q3))
+    print('Minimun : {}'.format(min))
+    print('Maximum : {}'.format(max))
+    print('Skewness : {}'.format(Skewness))
+    print('Kurtosis : {}'.format(Kurtosis))
+    print('Shapiro : {}'.format(Shapiro))
+    print('Normality Test : {}'.format(normality_test))
+
+    dynamic_bins=int(np.cbrt(len(df[col]))*2)
+
+    plt.figure(figsize=(15,8))
+    sns.distplot(df[col],bins=dynamic_bins)
+    plt.show()
+    plt.figure(figsize=(15,8))
+    sns.boxplot(df[col])
+    plt.show()
--- basic_exploration.py
+++ basic_exploration.py
@@ -64,10 +64,66 @@ def eda(df):
     ---------------
     None
 
+    print:
+    ---------------
+    print error message
+
     show:
     ---------------
     eda related plots
     '''
+    # variable declarartion
+    numeric_columns,object_columns=[],[]
+    categorical_list,non_categorical_list=[],[]
 
+    # column classification (numeric/non-numeric)
+    try:
+        numeric_columns=list(df.describe(exclude='object').columns)
+    except Exception:
+        print('numeric columns not exists.')
+    try:
+        object_columns=list(df.describe(include='object').columns)
+    except Exception:
+        print('non numeric columns not exists.')
 
+    # column classification (categorical/non-categorical)
+    if len(numeric_columns)>0:
+        for col_name in numeric_columns:
+            if df[col_name].nunique()>len(df[col_name])//4:
+                non_categorical_list.append(col_name)
+            else:
+                categorical_list.append(col_name)
+    
+    # append non-numerical columns to categorical columns list  
+    categorical_list=categorical_list+object_columns
+
+    # plot histogram and boxplot for continuous numerical columns (non-categorical)
+    if len(non_categorical_list)>0:
+        for i in non_categorical_list:
+            fig,(ax1,ax2)=plt.subplots(1,2,figsize=(9,5))
+            fig.suptitle('Plots of {} column'.format(i))
+            ax1.hist(df[i])
+            ax1.set_title('Histogram')
+            ax2.boxplot(df[i])
+            ax2.set_title('Boxplot')
+            fig.show()
+
+    # plot pie chart, bar graph and line plot for categorical columns
+    if len(categorical_list)>0:
+        for i in categorical_list:
+            fig,(ax1,ax2,ax3)=plt.subplots(1,3,figsize=(12,5))
+            fig.suptitle('Plots of {} column'.format(i))
+            ax1.pie(df[i].value_counts(),autopct='%2.2f')
+            ax1.set_title('Pie-Chart')
+            ax2.bar(df[i].index,df[i])
+            ax2.set_title('Bar-Graph')
+            ax3.plot(df[i])
+            ax3.set_title('Line-Plot')
+            fig.show()
+
+    # print('numeric',numeric_columns)
+    # print('non-numeric',object_columns)
+    # print('noncat',non_categorical_list)
+    # print('cat',categorical_list)
+    
     
